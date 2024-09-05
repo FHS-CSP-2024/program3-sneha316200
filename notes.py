f31be00adbf655f5083152fe94973b33c09217ec
sneha316200 <musalaesneha@gmail.com>
2024-09-05 15:00:39 +0000

    I finished my notes

--- notes.py
+++ notes.py
@@ -8,6 +8,57 @@
 #* You will understand the difference between strings, integers and floating point numbers
 
 
+myXVAL = 19 
+myxval = 7 
+
+print(myXVAL)
+
+name = "chuck"
+number = 100 
+newNumber = "100"
+
+print(number/2)
+#casting example 
+print (int(newNumber)/2)
+
+myFloat = 3.54
+
+num1 = 100
+num2 = 75
+num3 = 967 
+
+avg = (num1 + num2 + num3 )/3
+
+print(f"Average:{avg}")
+print(avg)
+print("Average", avg)
+print("Average"+ str(avg))
+
+
+beds = 4 
+bath = 3 
+address = "365 folsom high"
+city = "folsom"
+zip = 95630 
+rent = 7000
+
+print (f"House for rent at {address} in {city}")
+print (f"\t {beds} bedrooms, {bath} bathrooms")
+print (f"\t rent is ${rent}\month")
+
+#File path example 
+print("I have a file located at: C:\\Users\\mrJohnson\\Documen...")
+
+
+
+
+
+
+
+
+
+
+
 
 ## Live Demo ##
 #
